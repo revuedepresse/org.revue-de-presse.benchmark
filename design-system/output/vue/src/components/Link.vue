3a18ca728fc900aa5ce8bd866ec58d474aee1430
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-06 16:27:09 +0200

    feat(design-system): add Link atom

---
+++ design-system/output/vue/src/components/Link.vue
@@ -0,0 +1,42 @@
+<template>
+  <a
+    :class="`rdp-link rdp-link--${variant ?? 'underline'}`"
+    :href="href"
+    :target="external ? '_blank' : undefined"
+    :rel="external ? 'noopener noreferrer' : undefined"
+  >
+    <template v-if="labelKey">
+      {{ t(labelKey) }}
+    </template>
+
+    <template v-else>
+      {{ label ?? "" }}
+    </template>
+    <component :is="'style'">{{
+      `
+        .rdp-link {
+          color: var(--color-brand-active);
+          text-decoration: none;
+          font-family: 'Roboto', sans-serif;
+        }
+        .rdp-link--underline { text-decoration: underline; }
+        .rdp-link--plain { color: inherit; }
+        .rdp-link:hover { color: var(--color-brand); }
+      `
+    }}</component></a
+  >
+</template>
+
+<script setup lang="ts">
+import { t } from "../utils/i18n";
+
+type LinkProps = {
+  href: string;
+  labelKey?: string;
+  label?: string;
+  external?: boolean;
+  variant?: "underline" | "inline" | "plain";
+};
+
+const props = defineProps<LinkProps>();
+</script>
