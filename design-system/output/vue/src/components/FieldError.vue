1a890336eaa178bb344c6c2bc1427c863a93e425
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-06 16:27:10 +0200

    feat(design-system): add FieldError atom

---
+++ design-system/output/vue/src/components/FieldError.vue
@@ -0,0 +1,36 @@
+<template>
+  <template v-if="!!(messageKey || message)">
+    <p class="rdp-field-error" role="alert">
+      <template v-if="messageKey">
+        {{ t(messageKey, vars) }}
+      </template>
+
+      <template v-else>
+        {{ message ?? "" }}
+      </template>
+      <component :is="'style'">{{
+        `
+          .rdp-field-error {
+            font-size: var(--font-size-publication-date);
+            color: var(--form-error-fg);
+            margin: 0;
+            padding-left: var(--separation-1);
+            font-family: 'Roboto', sans-serif;
+          }
+        `
+      }}</component>
+    </p>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { t } from "../utils/i18n";
+
+type FieldErrorProps = {
+  messageKey?: string;
+  message?: string;
+  vars?: Record<string, string | number>;
+};
+
+const props = defineProps<FieldErrorProps>();
+</script>
