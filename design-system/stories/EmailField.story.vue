0b0271d6dda6016f80a5caf9bf12d3c812d388b4
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-06 16:47:03 +0200

    feat(design-system): add Histoire stories for all M1 atoms

---
+++ design-system/stories/EmailField.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import EmailField from '@design-system/components/EmailField.vue';
+const value = ref('');
+</script>
+<template>
+  <Story title="Atoms/EmailField">
+    <Variant title="default"><EmailField name="e" :value="value" @change="value = $event" /></Variant>
+    <Variant title="error"><EmailField name="e" value="malformed" error /></Variant>
+  </Story>
+</template>
