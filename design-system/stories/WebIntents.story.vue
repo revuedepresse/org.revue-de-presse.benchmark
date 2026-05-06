4c305a22696e9f995415479809cd1407989b1dff
Thierry Marianne <thierry@marianne.io>
2026-05-06 18:39:47 +0200

    feat(design-system): add Histoire stories for M2 molecules

---
+++ design-system/stories/WebIntents.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import WebIntents from '@design-system/components/WebIntents.vue';
+</script>
+
+<template>
+  <Story title="Molecules/WebIntents">
+    <Variant title="default"><WebIntents post-id="example-1" /></Variant>
+  </Story>
+</template>
