fc8bb7aab469457d4d7ebcc643c874e36c334a7a
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-06 16:47:03 +0200

    feat(design-system): add Histoire stories for all M1 atoms

---
+++ design-system/stories/MediaPlaceholder.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MediaPlaceholder from '@design-system/components/MediaPlaceholder.vue';
+</script>
+<template>
+  <Story title="Atoms/MediaPlaceholder">
+    <Variant title="270x160"><MediaPlaceholder :width="270" :height="160" /></Variant>
+    <Variant title="aspect 16/9"><MediaPlaceholder aspect-ratio="16 / 9" /></Variant>
+  </Story>
+</template>
---
+++ design-system/stories/PasswordField.story.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import PasswordField from '@design-system/components/PasswordField.vue';
+const value = ref('');
+</script>
+<template>
+  <Story title="Atoms/PasswordField">
+    <Variant title="login"><PasswordField name="p" :value="value" @change="value = $event" mode="login" /></Variant>
+    <Variant title="new (with helper)"><PasswordField name="p" :value="value" @change="value = $event" mode="new" /></Variant>
+    <Variant title="error"><PasswordField name="p" value="short" mode="new" error /></Variant>
+  </Story>
+</template>
