dac33d0001112b3871276cd32b500ce1813c6021
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-28 14:36:02 +0200

    native(androidApp): expose Compose testTag as resource-id for Maestro lookup

--- native/androidApp/src/androidMain/kotlin/org/revue_2_presse/android/MainActivity.kt
+++ native/androidApp/src/androidMain/kotlin/org/revue_2_presse/android/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import org.koin.compose.koinInject
 import org.revue_2_presse.design.theme.RdpTheme
 import org.revue_2_presse.domain.repositories.HighlightsRepository
@@ -15,11 +18,12 @@ import org.revue_2_presse.ui.nav.NavGraph
 import org.revue_2_presse.ui.nav.rememberRdpNavController
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RdpTheme {
-                Scaffold { _ ->
+                Scaffold(modifier = Modifier.semantics { testTagsAsResourceId = true }) { _ ->
                     val nav = rememberRdpNavController()
                     val highlights = koinInject<HighlightsRepository>()
                     val sources = koinInject<SourcesRepository>()
