d67111201ed89122f9fb51f1d4390dcba1966a8a
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-28 11:47:34 +0200

    native(data): Koin DataModule + installIdModule expect/actual
    Add explicit constructor to KtorClientFactory expect class so commonMain
    can instantiate it via Koin's single<> DSL.

--- native/data/src/commonMain/kotlin/org/revue_2_presse/data/api/KtorClientFactory.kt
+++ native/data/src/commonMain/kotlin/org/revue_2_presse/data/api/KtorClientFactory.kt
@@ -3,6 +3,6 @@ package org.revue_2_presse.data.api
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 
-expect class KtorClientFactory {
+expect class KtorClientFactory() {
     fun create(extraConfig: HttpClientConfig<*>.() -> Unit = {}): HttpClient
 }
